9d46d2800d2ab7d432ecec675f0ce7bbc3359c4a
PushEax <pusheax.vk@gmail.com>
2025-05-02 17:23:54 +0300

    add loading and errror status

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -10,6 +10,8 @@ const slice = createSlice({
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+    loading: false,
+    error: null,
   },
   reducers: {
     addContact: (state, action) => {
