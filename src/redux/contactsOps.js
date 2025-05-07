f6f0995ab50d3ca69fae21f0a1a879670b76faec
PushEax <pusheax.vk@gmail.com>
2025-05-07 20:39:07 +0300

    reject error message

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -4,7 +4,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = BASE_URL;
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-  const response = await axios.get('/contacts');
-  return response.data;
-});
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
